94d731e47a3520fb7659477cda98f6866c66456d
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-03-24 16:44:30 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+
+      <h3 className="font-bold text-6xl">Hello</h3>
+    </div>
+  );
+};
+
+export default App;
