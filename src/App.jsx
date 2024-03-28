f0a95ff1494df6aec94ec4c2d8ec28a5deef96b8
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-03-28 20:46:25 +0530

    add routes for RouterProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,53 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Header, Footer, LoadingComponent } from "./components/index";
 import authService from "./services/auth.service";
+import postService from "./services/post.service";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import { setLoading, setPosts } from "./store/postSlice";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  // const [fullname, setFullname] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [posts, setPosts] = useState([]);
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   const response = await authService.currentUsersPosts();
-  //   // const res = await authService.loginUser({ email, password });
-  //   // console.log(res);
-  //   // const response = await authService.currentUsersPosts({
-  //   //   // email,
-  //   //   // // fullname,
-  //   //   // password,
-  //   // });
-  //   console.log(response);
-  //   // setPosts(response.data)
-  // };
+  const loading = useSelector((state) => state.post.loading);
+  const dispatch = useDispatch();
 
-  return;
-  // <div>
-  //   <form onSubmit={handleSubmit}>
-  //     {/* <label>fullname</label>
-  //     <input
-  //       value={fullname}
-  //       type="text"
-  //       onChange={(e) => setFullname(e.target.value)}
-  //     /> */}
-  //     <label>email</label>
-  //     <input
-  //       value={email}
-  //       type="text"
-  //       onChange={(e) => setEmail(e.target.value)}
-  //     />
-  //     <label>password</label>
-  //     <input
-  //       type="text"
-  //       value={password}
-  //       onChange={(e) => setPassword(e.target.value)}
-  //     />
-  //     <button type="submit">submit</button>
-  //   </form>
-  // </div>
+  useEffect(() => {
+    authService
+      .currentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+          postService.getAllPosts().then((posts) => {
+            if (posts) dispatch(setPosts(posts));
+          });
+        } else dispatch(logout());
+      })
+      .finally(() => {
+        dispatch(setLoading(false));
+      });
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-300 font-rubik">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-300 font-rubik">
+      <div className="w-full block">
+        <Header />
+        <main>
+          <LoadingComponent />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  );
 };
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,132 @@ import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import AddPost from "./Pages/AddPost.jsx";
+import EditPost from "./Pages/EditPost.jsx";
+import Login from "./Pages/Login.jsx";
+import { AuthLayout } from "./components/index.js";
+import Signup from "./Pages/Signup.jsx";
+import Post from "./Pages/Post.jsx";
+import UsersPosts from "./Pages/UsersPosts.jsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout isAuthenticated={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout isAuthenticated={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout isAuthenticated>
+            {" "}
+            <UsersPosts />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout isAuthenticated>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout isAuthenticated>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
+]);
+
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//     <Route path="/" element={<App />}>
+//       <Route path="/" element={<Home />} />
+
+//       <Route
+//         path="/all-posts"
+//         element={
+//           <AuthLayout isAuthenticated>
+//             <AllPosts />
+//           </AuthLayout>
+//         }
+//       >
+//         <Route path="/post/:slug" element={<Post />} />
+//       </Route>
+
+//       <Route
+//         path="/add-post"
+//         element={
+//           <AuthLayout isAuthenticated>
+//             <AddPost />
+//           </AuthLayout>
+//         }
+//       />
+//       <Route
+//         path="/signup"
+//         element={
+//           <AuthLayout isAuthenticated={false}>
+//             <Signup />
+//           </AuthLayout>
+//         }
+//       />
+//       <Route
+//         path="/edit-post/:slug"
+//         element={
+//           <AuthLayout isAuthenticated>
+//             <EditPost />
+//           </AuthLayout>
+//         }
+//       />
+//       <Route path="/post/:slug" element={<Post />} />
+//       <Route
+//         path="/login"
+//         element={
+//           <AuthLayout isAuthenticated={false}>
+//             <Login />
+//           </AuthLayout>
+//         }
+//       />
+//     </Route>
+//   )
+// );
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
