0a9d51ab489147b04107cc0bc4dceb84e5da3c92
Kamlesh-Chowdary <kamleshchoyal143@gmail.com>
2024-03-30 15:44:08 +0530

    fix: backend url update

--- src/helpers/axiosInstance.js
+++ src/helpers/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://blogburst-backend-974c.onrender.com",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
